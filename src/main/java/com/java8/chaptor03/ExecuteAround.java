cdd10dbd8249e8853eaf47cc4466f26f29b6567b
alexpdh <alexpdh@163.com>
2017-12-07 00:55:38 +0800

    添加环绕模式示例

---
+++ src/main/java/com/java8/chaptor03/ExecuteAround.java
@@ -0,0 +1,43 @@
+package com.java8.chaptor03;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * 环绕执行模式
+ *
+ * @author pengdh
+ * @date 2017/12/07
+ */
+public class ExecuteAround {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(processLimitFile());
+
+		String oneLine = processFile(b -> b.readLine());
+		System.out.println(oneLine);
+		String twoLine = processFile(b -> b.readLine() + b.readLine());
+		System.out.println(twoLine);
+	}
+
+	public static String processLimitFile() throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+			return br.readLine();
+		}
+	}
+
+	public static String processFile(BufferedReaderProcessor p) throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+			return p.process(br);
+		}
+	}
+
+	/**
+	 * Lambda 仅可以用于上线问是函数式接口的情况，
+	 * 需要创建一个匹配 BufferedReader -> String
+	 */
+	public interface BufferedReaderProcessor {
+		String process(BufferedReader b) throws IOException;
+	}
+}
